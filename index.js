55a6ec5fd973cf02c42f275821cae51590fea9c4
bielvbcg <gabrielvbcallegario@gmail.com>
2022-01-28 12:48:18 -0300

    fazendo rota dos participants

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 import express from "express"
 import cors from "cors"
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br.js';
+import { MongoClient } from "mongodb"
 
 const server = express()
+server.use(express.json)
 server.use(cors())
 
+const mongoClient = new MongoClient("mongodb://localhost:27017")
 
+server.post("/participants", async (req, res) => {
+  try {
+    await mongoClient.connect()
+    const db = mongoClient.db("bate-papo-uol")
+    const usuariosCollection = db.collection("usuarios")
+    const mensagensCollection = db.collection("mensagens")
+
+    await usuariosCollection.insertOne(
+      {
+        name: req.body.name,
+        lastStatus: Date.now()
+      })
+
+    await mensagensCollection.insertOne(
+      {
+        from: req.body.name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs().format("HH:mm:ss")
+      })
+
+    res.sendStatus(201)
+    mongoClient.close()
+
+  } catch (error) {
+    res.send(error)
+    mongoClient.close()
+  }
+})
 
 
 server.listen(5000)
