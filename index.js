865521e34361a32a7666dcf0f236c7c3fcbaff7f
bielvbcg <gabrielvbcallegario@gmail.com>
2022-01-28 17:16:28 -0300

    post e get da rota /participants funcionais com validaçoes de erro

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 import express from "express"
 import cors from "cors"
 import dayjs from 'dayjs';
-import 'dayjs/locale/pt-br.js';
 import { MongoClient } from "mongodb"
+import joi from "joi"
 
 const server = express()
-server.use(express.json)
+server.use(express.json())
 server.use(cors())
 
-const mongoClient = new MongoClient("mongodb://localhost:27017")
+let mongoClient = new MongoClient("mongodb://localhost:27017")
+
+const usuarioSchema = joi.object({ name: joi.string().required() })
 
 server.post("/participants", async (req, res) => {
+
+  const validacao = usuarioSchema.validate(req.body, { abortEarly: false })
+  if (validacao.error) {
+    const erros = validacao.error.details.map(detail => detail.message)
+
+    res.status(422).send(erros)
+    return
+  }
+
   try {
     await mongoClient.connect()
     const db = mongoClient.db("bate-papo-uol")
     const usuariosCollection = db.collection("usuarios")
     const mensagensCollection = db.collection("mensagens")
 
-    await usuariosCollection.insertOne(
-      {
-        name: req.body.name,
-        lastStatus: Date.now()
-      })
+    let usuarioExistente = false
+    const usuarios = await usuariosCollection.find({}).toArray()
+    usuarios.map(item => { if (item.name === req.body.name) usuarioExistente = true })
+
+    if (usuarioExistente) {
+      res.sendStatus(409)
+      mongoClient.close()
+      return
+    }
+
+    await usuariosCollection.insertOne({ name: req.body.name, lastStatus: Date.now() })
 
     await mensagensCollection.insertOne(
       {
@@ -37,6 +54,22 @@ server.post("/participants", async (req, res) => {
 
   } catch (error) {
     res.send(error)
+    mongoClient.close()
+  }
+})
+
+server.get("/participants", async (req, res) => {
+  try {
+    await mongoClient.connect()
+    const db = mongoClient.db("bate-papo-uol")
+    const usuariosCollection = db.collection("usuarios")
+    const usuarios = await usuariosCollection.find({}).toArray()
+
+    res.status(200).send(usuarios)
+    mongoClient.close()
+
+  } catch (error) {
+    res.sendStatus(500)
     mongoClient.close()
   }
 })
